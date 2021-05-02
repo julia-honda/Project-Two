3173b5c0ad4047caf3e42f78cbfacda008471676
David Singer <dsinger1@hawk.iit.edu>
2021-05-02 03:59:00 -0500

    A working javascript for about us page

--- js/site.js
+++ js/site.js
@@ -1,51 +1,30 @@
 /*JavaScript added on this page */
 
-/*
-  var History = document.getElementById("about-us-history-title").onclick = function() {
+
+
+  var History = document.getElementById("about-us-content-title").onclick = function(){
     showhideHistory()
   };
-
-  var Const = document.getElementById("about-us-const-title").onclick = function() {
-    document.getElementById("about-us-constitition").style.display = "block";
-  };
-
-  var Journal = document.getElementById("about-us-journal-title")
-
   function showhideHistory() {
-    document.getElementById("about-us-history").style.display = "block"
-  }
-  */
-/*
-  function toggleit(){
-    var Const = document.getElementById("about-us-const-title").onclick;
-    if (Const.style.display === "none") {
-      Const.style.display = "block";
-    } else{
-      text.style.display = "none"
+    if (document.getElementById("about-us-content-title").onclick){
+      if(document.getElementById("about-us-content").style.display === 'none'){
+        document.getElementById("about-us-content").style.display = 'block';
+      }else {
+        document.getElementById("about-us-content").style.display = 'none';
+      }
     }
   }
-*/
-/*
-  var Test = document.getElementById("about-us-const-title");
-  var Second = document.getElementById("about-us-constitution");
-  Test.addEventListener('click', function(){
-    Second.toggleDisplayById("about-us-constitution");
-    if (Second.style.display === "none") {
-      Second.style.display = "block";
-    } else {
-      Second.style.display = "none";
-    }
-  });
-*/
 
   var History = document.getElementById("about-us-history-title").onclick = function(){
     showhideHistory()
   };
   function showhideHistory() {
     if (document.getElementById("about-us-history-title").onclick){
-      document.getElementById("about-us-history").style.display = 'block';
-    } else {
-      document.getElementById("about-us-history").style.display = 'none'
+      if(document.getElementById("about-us-history").style.display === 'none'){
+        document.getElementById("about-us-history").style.display = 'block';
+      }else {
+        document.getElementById("about-us-history").style.display = 'none';
+      }
     }
   }
 
@@ -54,20 +33,11 @@
   };
   function showhideConst() {
     if (document.getElementById("about-us-const-title").onclick){
-      document.getElementById("about-us-constitition").style.display = 'block';
-    } else {
-      document.getElementById("about-us-constitition").style.display = 'none'
-    }
-  }
-
-  var History = document.getElementById("about-us-history-title").onclick = function(){
-    showhideHistory()
-  };
-  function showhideHistory() {
-    if (document.getElementById("about-us-history-title").onclick){
-      document.getElementById("about-us-history").style.display = 'block';
-    } else {
-      document.getElementById("about-us-history").style.display = 'none'
+      if(document.getElementById("about-us-constitition").style.display === 'none'){
+        document.getElementById("about-us-constitition").style.display = 'block';
+      }else {
+        document.getElementById("about-us-constitition").style.display = 'none';
+      }
     }
   }
 
@@ -76,8 +46,10 @@
   };
   function showhideJournal() {
     if (document.getElementById("about-us-journal-title").onclick){
-      document.getElementById("about-us-journal").style.display = 'block';
-    } else {
-      document.getElementById("about-us-journal").style.display = 'none'
+      if(document.getElementById("about-us-journal").style.display === 'none'){
+        document.getElementById("about-us-journal").style.display = 'block';
+      }else {
+        document.getElementById("about-us-journal").style.display = 'none';
+      }
     }
   }
